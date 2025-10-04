9f1fb6b6c0317be1099e0a93d906076ec986b894
Mochammad Syahrul Kurniawan <mochrul.kurniawan@gmail.com>
2025-10-05 00:44:49 +0800

    add auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,24 +1,22 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-function requireAuth(req, res, next) {
+const authenticate = (role) => (req, res, next) => {
   const token = req.header('Authorization');
-  if (!token) return res.status(401).json({ message: 'Unauthorized' });
+  if (!token) return res.status(401).json({ message: 'Unauthorized!' });
 
   try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = payload;
-    return next();
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+
+    if (req.user.role !== role) {
+      return res.status(403).json({ messages: 'Unauthorized access!' });
+    }
+
+    next();
   } catch (error) {
-    return res.status(401).json({ message: 'Invalid token' });
+    res.status(400).json({ message: 'Invalid Token' });
   }
-}
+};
 
-function requireOwner(req, res, next) {
-  if (req.user?.role !== 'owner') {
-    return res.status(403).json({ message: 'Forbidden' });
-  }
-  next();
-}
-
-module.exports = { requireAuth, requireOwner };
+module.exports = { authenticate };
