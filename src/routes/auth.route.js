5fe0a66c5330b84f1bfbb62bbb4da157c9d58c1d
Mochammad Syahrul Kurniawan <mochrul.kurniawan@gmail.com>
2025-10-05 01:18:27 +0800

    add rate limit to signin

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,9 @@
-const router = require('express').Router();
 const auth = require('../controllers/auth.controller');
 const { authenticate } = require('../middlewares/auth');
+const { limiter } = require('../middlewares/rateLimit');
+const router = require('express').Router();
 
-router.post('/signin', auth.signin);
+router.post('/signin', limiter, auth.signin);
 router.get('/keep-signed-in', authenticate('owner'), auth.keepSignedIn);
 
 module.exports = router;
