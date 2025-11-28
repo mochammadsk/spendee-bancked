9962f60289129d668e0a02a93ac57fbd0016555b
Mochammad Syahrul Kurniawan <mochrul.kurniawan@gmail.com>
2025-11-29 04:53:42 +0800

    feat: add routes for signup

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,12 @@
 const auth = require('../controllers/auth.controller');
 const { authenticate } = require('../middlewares/auth');
 const { limiter } = require('../middlewares/rateLimit');
+const { validate } = require('../middlewares/validate');
+const { signupValidator } = require('../validators/authValidator');
 const router = require('express').Router();
 
+router.post('/signup', limiter, validate(signupValidator), auth.signup);
 router.post('/signin', limiter, auth.signin);
-router.get('/keep-signed-in', authenticate('owner'), auth.keepSignedIn);
+router.get('/keep-signed-in', authenticate, auth.keepSignedIn);
 
 module.exports = router;
