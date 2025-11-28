dfdf09fcbd26e94c5e2de3edb341a6464201b153
Mochammad Syahrul Kurniawan <mochrul.kurniawan@gmail.com>
2025-11-29 04:54:27 +0800

    feat: create user OTP schema

---
+++ src/models/userOtp.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose');
+
+const userOtpSchema = new Schema(
+  {
+    user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    otp: { type: String, required: true },
+    expires_at: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
+
+userOtpSchema.method('toJSON', function () {
+  const { __v, _id, ...object } = this.toObject();
+  return { id: _id, ...object };
+});
+
+module.exports = model('UserOtp', userOtpSchema);
