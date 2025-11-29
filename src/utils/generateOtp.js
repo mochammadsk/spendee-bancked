6c60ce9111f63c70eb6827d73f61094898411519
Mochammad Syahrul Kurniawan <mochrul.kurniawan@gmail.com>
2025-11-29 23:45:12 +0800

    fix: update funct generate OTP

--- src/utils/generateOtp.js
+++ src/utils/generateOtp.js
@@ -1,11 +1,15 @@
-function generateOtp(length = 6) {
+const bcrypt = require('bcrypt');
+
+async function generateOtp() {
   let otp = '';
-
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < 6; i++) {
     otp += Math.floor(Math.random() * 10);
-  }
+  } // create OTP (6 digit)
 
-  return otp;
+  const otpHash = await bcrypt.hash(otp, 10); // hash OTP
+  const expires_at = new Date(Date.now() + 5 * 60 * 1000); // set expiration time (5 minutes)
+
+  return { otp, otpHash, expires_at };
 }
 
 module.exports = generateOtp;
