e41cbd395a502328ddefd07e2141e933332bd2a7
Mochammad Syahrul Kurniawan <mochrul.kurniawan@gmail.com>
2025-11-29 21:07:29 +0800

    fix: update styling of email messages

--- src/utils/sendOtp.js
+++ src/utils/sendOtp.js
@@ -17,28 +17,38 @@ async function sendOtp(email, otp) {
     to: email,
     subject: 'Your verification code',
     html: `
-      <div style="font-family: Arial, sans-serif; line-height: 1.6;">
-        <div style="max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px; background-color: #f9f9f9;">
-          <h2 style="text-align:center; color:#333;">Email Verification</h2>
-          <p style="color:#333;">
-            Use the following verification code to complete your registration:
-          </p>
-          <div style="text-align:center; margin: 20px 0;">
-            <span style="display:inline-block; padding: 10px 20px; font-size: 24px; letter-spacing: 4px; background:#007BFF; color:#fff; border-radius: 5px;">
-              ${otp}
-            </span>
-          </div>
-          <p style="color:#333;">
-            This code will expire in 10 minutes.
-          </p>
-          <p style="color:#333;">
-            If you did not create an account, please ignore this email.
-          </p>
-          <p style="color:#333;">Best regards,<br/>
-            <b>Spendee Team</b>
-          </p>
+      <div style="font-family: Arial, sans-serif; line-height: 1.6">
+      <div
+        style="
+          max-width: 600px;
+          margin: 0 auto;
+          padding: 20px;
+          border: 1px solid #ddd;
+          border-radius: 10px;
+          background-color: #f9f9f9;
+        "
+      >
+        <h2 style="text-align: center; color: #333">Email Verification</h2>
+        <p style="color: #333">
+          Use the following verification code to complete your registration:
+        </p>
+        <div style="text-align: center; margin: 20px 0">
+          <span
+            style="display: inline-block; font-size: 24px; letter-spacing: 4px"
+          >
+            ${otp}
+          </span>
         </div>
+        <p style="color: #333">This code will expire in 5 minutes.</p>
+        <p style="color: #333">
+          If you did not create an account, please ignore this email.
+        </p>
+        <p style="color: #333">
+          Best regards,<br />
+          <b>Spendee Team</b>
+        </p>
       </div>
+    </div>
     `,
   };
 
